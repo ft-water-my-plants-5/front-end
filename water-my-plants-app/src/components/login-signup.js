cc785d827eb2d031c6e8e8a5f3af093cc9bba238
MarkTrepanier <marktrepanier.cajigas@gmail.com>
2021-09-21 16:20:34 -0400

    login basic styling. nonfunct btn

--- water-my-plants-app/src/components/login-signup.js
+++ water-my-plants-app/src/components/login-signup.js
@@ -1,8 +1,32 @@
 import React from 'react';
+import styled from 'styled-components';
+import Styled from 'styled-components'
+import theme from '../theme';
+
+const LoginStyle= Styled.div`
+	display: flex;
+	flex-flow: row wrap;
+	justify-content: center;
+	align-items: center;
+	div{
+		display: flex;
+		flex-direction column;
+		border: ${props=>props.theme.colors.greenSheen} solid 2px;
+		border-radius: 3px;
+		box-shadow: 1px 2px;
+		margin-top:10px;
+		margin-left:10px;
+		margin-right:10px;
+		button{
+			width:30%;
+			align-self:center;
+		}
+	}
+`
 
  export default function LoginSignup(){
 	return(
-		<>
+		<LoginStyle>
 			<div className='login'>
 				<h3>login</h3>
 				<label>{'email or username '}
@@ -12,8 +36,8 @@ import React from 'react';
 					<input type='text' name='password'/>
 				</label>
 				{/*todo: setup validation*/}
-				{/*todo: setup loginbtn*/}
 				{/*todo: loginbtn routes to homepage*/}
+				<button>Log in</button>
 			</div>
 
 			<div className='signup'>
@@ -27,10 +51,20 @@ import React from 'react';
 				<label>{'password '}
 					<input type='text' name='password'/>
 				</label>
+				<button>sign up</button>
 				{/*todo: setup validation*/}
-				{/*todo: setup signupbtn*/}
 				{/*todo: signupbtn routes to homepage?*/}
 			</div>
-		</>
+		</LoginStyle>
 	)
 }
+
+/*user
+dummyData
+userName
+phoneNumber
+password
+
+
+
+*/
