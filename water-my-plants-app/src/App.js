2e4c612bce4a6e593fac08fbc56a2c8cb0176547
Kat <kyevsukov@gmail.com>
2021-09-21 10:16:21 -0400

    adjusted routing to login signup form, wrapped with router

--- water-my-plants-app/src/App.js
+++ water-my-plants-app/src/App.js
@@ -1,38 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
-import Axios from 'axios';
-import React, {useState, useEffect} from 'react';
-import LoginSignup from 'login-signup';
-import {Route} from 'react-router-dom';
+import logo from "./logo.svg";
+import "./App.css";
+import Axios from "axios";
+import React, { useState, useEffect } from "react";
+import LoginSignup from "../src/login-signup.js";
+import { Route, Switch } from "react-router-dom";
 
 //filler state
-const [plants, setPlants] = useState([]);
-const [plant, setPlant] = useState()
-const [user, setUser] =useState()
+// const [plants, setPlants] = useState([]);
+// const [plant, setPlant] = useState();
+// const [user, setUser] = useState();
 
-//SET DATA
-useEffect(
-  Axios.get('dummydata.api')
-    .then(res=>{
-      console.log(res.data);
-      setData(res.data);
-    })
-  .catch(er=>{
-    console.error(er);			
-
-    }),[])
+// //SET DATA
+// useEffect(
+//   Axios.get("dummydata.api")
+//     .then((res) => {
+//       console.log(res.data);
+//       setData(res.data);
+//     })
+//     .catch((er) => {
+//       console.error(er);
+//     }),
+//   []
+// );
 
 //RETURN HTML
 function App() {
   return (
     <div className="App">
-			<Route>
-				<LoginSignup/>
-			</Route>
-
-			<Route>
-				{/*<SomethingElse/>*/}
-			</Route>
+      <Switch>
+        <Route path="/login">
+          <LoginSignup />
+        </Route>
+        <Route path="/">
+          <LoginSignup />
+        </Route>
+      </Switch>
     </div>
   );
 }
