6f04c0328c18c11f03e8b927332d3a3b31884eb4
Kat <kyevsukov@gmail.com>
2021-09-22 14:00:00 -0400

    added and styed nav bar

--- water-my-plants-app/src/components/Header.js
+++ water-my-plants-app/src/components/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import NavBar from './Navbar'
 
 const StyledHeader = styled.header`
   background-image: url("https://images.unsplash.com/photo-1485134532658-d720895a3b5e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1746&q=80");
@@ -7,6 +8,7 @@ const StyledHeader = styled.header`
   display: flex;
   height: 20vh;
   padding: 0 1em;
+  justify-content: space-between;
 `;
 const StyledTitle = styled.h1`
   color: whitesmoke;
@@ -14,13 +16,13 @@ const StyledTitle = styled.h1`
   text-shadow: 2px 2px ${(props) => props.theme.colors.amazon};
   margin: auto 0;
 `;
-const StyledNavigation = styled.nav``;
+
 
 export default function Header() {
   return (
     <StyledHeader>
       <StyledTitle>Water My Plants</StyledTitle>
-      <StyledNavigation></StyledNavigation>
+      <NavBar />
     </StyledHeader>
   );
 }
